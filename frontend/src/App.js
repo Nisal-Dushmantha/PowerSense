2ad620404108c3b069c8bdfb3d835ae650cd3d93
Lawa-777 <lawanyanisal@gmail.com>
2026-04-11 22:20:17 +0530

    vercel fixes 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,9 +16,6 @@ import { authService } from './services/authService';
 import { ThemeProvider } from './contexts/ThemeContext';
 import DevicesList from './components/Devices/DevicesList';
 import ConsumptionList from './components/energyConsumption/ConsumptionList';
-import DeviceChartsPage from './components/Devices/DeviceChartsPage';
-import EditDevice from './components/Devices/EditDevice';
-import AdminDashboard from './components/admin/AdminDashboard';
 
 // Initialize auth service
 authService.init();
